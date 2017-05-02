c98ab9d42722a79be5aebcceaf8ff7abb6689780
Myles Ieong Sai Hou <BI77@corp.dahsing.com>
2017-05-02 18:09:03 +0800

    Progress on adapting the C# code into C++

--- README.md
+++ README.md
@@ -6,3 +6,7 @@
     - note. The DLL should be proven with `$ file {dll_name}.dll` that its 32/64bit
     - note. The DLL must be put at the directory where the JVM runs. If not, change the command to this `$(project_home) java -Djna.library.path={path_to_dll_folder} -cp target/jna-demo_xxx.jar com.myles.demo.App` 
     - note. Run the JVM in cygwin-bash, if use git-bash, some system depenedency would be missed.
+- **2017-05-02**: 
+    - The original code is using C#, maybe we can try to **ping the g++ compile C# as dll** possibility? 
+    - Now reading the C++ tutorial, some retrofit will be made on the C# code if we stick to C++
+    - The deadline is monday update
--- c_playground/pingLM.cpp
+++ c_playground/pingLM.cpp
@@ -1,10 +1,77 @@
 #include "pingLM.h"
+#include "FreeImage.h"
  
 #include <stdexcept>
 
 using namespace std;
 
+struct DecodeResult
+{
+    char pdata_buf[];
+    int buf_len;
+    int data_len;
+};
+
+struct ImageData
+{
+   int img_w;
+   int img_h;
+   int row_bytes;
+   int bitcount;
+   int is_dark_pixel_bit1;
+   void * praw_data;
+};
+
 char * decode(char * filename)
 {
+    FREE_IMAGE_FORMAT fif = FREE_IMAGE_FORMAT.FIF_UNKNOWN;
+    fif = FreeImage.GetFileType(filename, 0);
+    if (fif == FREE_IMAGE_FORMAT.FIF_UNKNOWN)
+    {
+        fif = FreeImage.GetFIFFromFilename(filename);
+    }
+    FIBITMAP img = FreeImage.Load(fif, filename, 0);
+    ImageData pImageData = new ImageData();
+    pImageData.bitcount = FreeImage.GetBPP(img);
+
+
+    //If NOT IN (1,8,24) bit image, transfer to 24 bit.
+    if (pImageData.bitcount != 1 && pImageData.bitcount != 8
+        && pImageData.bitcount != 24)
+    {
+        img = FreeImage.ConvertTo24Bits(img);
+        pImageData.bitcount = FreeImage.GetBPP(img);
+    }
+    pImageData.is_dark_pixel_bit1 = 1;
+    pImageData.img_w = FreeImage.GetWidth(img);
+    pImageData.img_h = FreeImage.GetHeight(img);
+    pImageData.row_bytes = FreeImage.GetPitch(img);
+    pImageData.praw_data = FreeImage.GetBits(img);
+
+    int pImageDataSize = Marshal.SizeOf(pImageData);
+    IntPtr pImageDataIntPtr = Marshal.AllocHGlobal(pImageDataSize);
+    Marshal.StructureToPtr(pImageData, pImageDataIntPtr, true);
+
+    int pResultSize = Marshal.SizeOf(typeof(DecodeResult));
+    IntPtr pResultIntPtr = Marshal.AllocHGlobal(pResultSize);
+
+    //Decode
+    int result = LM_ImageData_Decode(pImageDataIntPtr, 0, ref pResultIntPtr, 1);
+    if (result == 1)
+    {
+        DecodeResult[] infos = new DecodeResult[1];
+        IntPtr ptr = (IntPtr)((UInt32)pResultIntPtr + 0 * pResultSize);
+        infos[0] = (DecodeResult)Marshal.PtrToStructure(ptr, typeof(DecodeResult));
+        string resultInfo = System.Text.Encoding.UTF8.GetString(infos[0].pdata_buf);
+        this.textBox1.Text = resultInfo;
+    }
+    else
+    {
+        MessageBox.Show(result+"");
+    }
+    FreeImage.Unload(img);
+    Marshal.FreeHGlobal(pResultIntPtr);   
+    Marshal.FreeHGlobal(pImageDataIntPtr);
+    
 	return filename;
 }
